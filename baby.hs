a466a7520cc2464e2b92fd95b152c77c783d9919
dineshr93 <dineshr93@gmail.com>
2014-05-20 23:51:21 +0530

    added lenght using :

--- baby.hs
+++ baby.hs
@@ -27,3 +27,14 @@ pickEvens xxs=[[x|x<-xs,even x]|xs<-xxs]
 factorial :: (Integral a) => a -> a  
 factorial 0 = 1  
 factorial n = n * factorial (n - 1)  
+
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y 
+
+
+lengthw :: (Num b) => [a] -> b  
+lengthw [] = 0  
+lengthw (_:xs) = 1 + lengthw xs  
